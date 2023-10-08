789f9fa856b5c253dd15c7d051bbf17bb1469980
Daniel Bindig <d.bindig@outlook.com>
2023-10-08 14:57:02 +1030

    Added POST route to api.js

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,14 @@ router.get("/notes", (req, res) => {
     response.json(data)
 });
 
+router.post("/notes", (req, res) => {
+    const newNote = request.body;
+    newNote.id = uuid();
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    data.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    response.json(data);
+})
 
 
 module.exports = router;

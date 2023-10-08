a19e647eab3746912a241996e61a096aab16db67
Daniel Bindig <d.bindig@outlook.com>
2023-10-08 12:09:50 +1030

    Added routes folder with the router files

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT}`)
+);

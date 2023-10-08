1296c9329aa9a2f042da2f220541bd276e27b110
Daniel Bindig <d.bindig@outlook.com>
2023-10-08 13:03:25 +1030

    Added connected html router to server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const htmlRouter = require("./routes/html")
+
+app.use('/', htmlRouter)
+
 app.listen(PORT, () =>
   console.log(`Listening on port ${PORT}`)
 );

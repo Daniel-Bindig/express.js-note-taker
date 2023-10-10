b4f4dcf476c7a0637b166d8d5a9c39cba7813dd2
Daniel Bindig <d.bindig@outlook.com>
2023-10-10 15:08:38 +1030

    Fixed bug

--- routes/api.js
+++ routes/api.js
@@ -5,24 +5,24 @@ const uuid = require('uuid');
 
 router.get("/notes", (req, res) => {
     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
-    response.json(data)
+    res.json(data)
 });
 
 router.post("/notes", (req, res) => {
-    const newNote = request.body;
-    newNote.id = uuid();
+    const newNote = req.body;
+    newNote.id = uuid.v4();
     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     data.push(newNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(data));
-    response.json(data);
+    res.json(data);
 })
 
-router.delete("/api/notes/:id", (request, response) => {
-let noteId = request.params.id.toString();
-let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-const newData = data.filter( note => note.id.toString() !== noteId );
-fs.writeFileSync('./db/db.json', JSON.stringify(newData));
-response.json(newData);
+router.delete("/notes/:id", (req, res) => {
+    let noteId = req.params.id.toString();
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    const newData = data.filter( note => note.id.toString() !== noteId );
+    fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+    res.json(newData);
 });
 
 module.exports = router;
